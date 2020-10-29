2181688b7725d1ff7303cd9a943cd5a6799cdfaa
Alex Holyk <alexholyk@Alexs-MacBook-Pro.local>
2020-10-29 11:25:27 +0200

    54

--- btre/settings.py
+++ btre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,9 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 MEDIA_URL = '/media/'
+
+
+# Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
